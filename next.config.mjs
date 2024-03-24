b8a8ee5fb07d2bf1b8e96e496ea3cd5440ac1776
Tristan Chin <tristan.chin@chintristan.io>
2024-03-24 01:45:28 -0400

    no-index preview deployments

--- next.config.mjs
+++ next.config.mjs
@@ -64,6 +64,22 @@ const nextConfig = {
 
     return config;
   },
+
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
+  },
 };
 
 export default withVanillaExtract(nextConfig);
