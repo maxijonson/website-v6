92da0f6841a5a1b9f824d54d1f0891f33b31d1d1
Tristan Chin <tristan.chin@chintristan.io>
2024-03-23 22:55:46 -0400

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,74 @@ import "./globals.css";
 import { fontSans } from "@/app/fonts";
 import ThemeProvider from "@/components/theme-provider";
 import DevUtils from "./components/dev-utils/dev-utils";
+import ogImage from "$/image/meta/og.png";
 
-export const metadata: Metadata = {
-  title: "Tristan Chin",
-  description:
-    "Personnal website of Tristan Chin, B. Eng. Software Engineering and Web Developer.",
+export const generateMetadata = (): Metadata => {
+  const description =
+    "Personal website of Tristan Chin, B. Eng. Software Engineering and Web Developer.";
+  const metadataBase = (() => {
+    switch (process.env.NODE_ENV) {
+      case "development":
+        return new URL("http://localhost:3000");
+      case "production":
+        return new URL("https://chintristan.io");
+      default:
+        if (process.env.VERCEL_URL) {
+          return new URL(`https://${process.env.VERCEL_URL}`);
+        }
+        return undefined;
+    }
+  })();
+
+  return {
+    metadataBase,
+    title: "Tristan Chin",
+    description,
+    authors: [{ name: "Tristan Chin" }],
+    keywords: [
+      "Web developer",
+      "Software engineering",
+      "Portfolio",
+      "Programmer",
+      "Tristan Chin",
+      "MaxiJonson",
+    ],
+    generator: "Next.js",
+    robots: {
+      index: true,
+      follow: true,
+    },
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      countryName: "Canada",
+      title: "Tristan Chin's Personal Website",
+      description,
+      images: [
+        {
+          url: ogImage.src,
+          alt: "Tristan Chin's Personal Website",
+          width: ogImage.width,
+          height: ogImage.height,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "@MaxiJonson",
+      creatorId: "1726834448",
+      description,
+      title: "Tristan Chin's Personal Website",
+      images: [
+        {
+          url: ogImage.src,
+          alt: "Tristan Chin's Personal Website",
+          width: ogImage.width,
+          height: ogImage.height,
+        },
+      ],
+    },
+  };
 };
 
 export const viewport: Viewport = {
