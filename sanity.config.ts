f51901ea51060661083ff3e263ff34f9d2d726bb
Tristan Chin <tristan.chin@chintristan.io>
2024-03-29 22:40:14 -0400

    studio workspaces

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,16 +3,34 @@
  */
 
 import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
+import { defineConfig, type Config } from "sanity";
+import { RobotIcon, RocketIcon } from "@sanity/icons";
 import { structureTool } from "sanity/structure";
 
-import { apiVersion, dataset, projectId } from "./sanity/env";
+import { apiVersion, projectId } from "./sanity/env";
 import { schema } from "./sanity/schema";
 
-export default defineConfig({
-  basePath: "/studio",
+const configBase = {
   projectId,
-  dataset,
   schema,
   plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
-});
+} satisfies Partial<Config>;
+
+export default defineConfig([
+  {
+    ...configBase,
+    dataset: "production",
+    basePath: "/studio",
+    name: "Production",
+    title: "Production",
+    icon: RocketIcon,
+  },
+  {
+    ...configBase,
+    dataset: "staging",
+    basePath: "/studio-staging",
+    name: "Staging",
+    title: "Staging",
+    icon: RobotIcon,
+  },
+]);
