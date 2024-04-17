895787415190dec30458235383ecd00f47c0bbeb
Tristan Chin <tristan.chin@chintristan.io>
2024-04-17 19:47:31 -0400

    Add BlogOverview component as base page for all blog post lists

---
+++ src/app/blog/components/blog-overview/blog-overview.tsx
@@ -0,0 +1,46 @@
+import Footer from "@/components/footer/footer";
+import BlogHeader from "../blog-header/blog-header";
+import type { BlogHeroProps } from "../blog-hero/blog-hero";
+import type { BlogSectionProps } from "../blog-section/blog-section";
+import BlogHero from "../blog-hero/blog-hero";
+import BlogSection from "../blog-section/blog-section";
+
+export interface BlogOverviewProps {
+  hero: BlogHeroProps;
+  sections: BlogSectionProps[];
+}
+
+const BlogOverview = ({ hero, sections }: BlogOverviewProps) => {
+  const postSlugs = new Set<string>();
+  const uniqueSections = sections.map((section) => ({
+    ...section,
+    posts: section.posts.filter((post) => {
+      if (postSlugs.has(post.slug)) {
+        return false;
+      }
+      postSlugs.add(post.slug);
+      return true;
+    }),
+  }));
+
+  return (
+    <>
+      <BlogHeader />
+      <main className="min-h-dvh">
+        <BlogHero {...hero} />
+
+        <div className="flex flex-col gap-4">
+          {uniqueSections.map(
+            (section) =>
+              section.posts.length > 0 && (
+                <BlogSection key={section.title} {...section} />
+              ),
+          )}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default BlogOverview;
