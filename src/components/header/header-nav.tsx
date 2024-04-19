909f133d8b2cda57eaf8c5cbeda7601f2df3af6f
Tristan Chin <tristan.chin@chintristan.io>
2024-04-19 17:23:26 -0400

    Simplify header-nav

--- src/components/header/header-nav.tsx
+++ src/components/header/header-nav.tsx
@@ -1,30 +1,18 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const HeaderNav = () => {
   return (
-    <div className="grow px-2">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/blog" legacyBehavior passHref>
-              <NavigationMenuLink
-                className={cn(navigationMenuTriggerStyle(), "bg-transparent")}
-              >
-                Blog
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+    <nav className={cn("grow px-2", "md:px-4")}>
+      <ul className="flex gap-2">
+        <li>
+          <Link href="/blog">
+            <Button variant="ghost">Blog</Button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
