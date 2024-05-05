1bbc0b7d9a1b1c3ccd5ed6fae3ee6762d605db82
Tristan Chin <tristan.chin@chintristan.io>
2024-05-04 21:22:45 -0400

    Update sitemap for blog

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,71 @@
 import { getBaseURL } from "@/utils/getBaseURL";
 import { MetadataRoute } from "next";
+import { getPosts } from "../../sanity/queries/post/getPosts";
+import { pick } from "../../sanity/groqd/selections/pick";
+import { postDetailsSelection } from "../../sanity/groqd/selections/post-details";
+import {
+  getRecentPosts,
+  getRecentPostsByCategoryId,
+  getRecentPostsByTagId,
+} from "../../sanity/queries/post/getRecentPosts";
+import { categoryDetailsSelection } from "../../sanity/groqd/selections/category-details";
+import { getCategories } from "../../sanity/queries/categories/getCategories";
+import { getTags } from "../../sanity/queries/tags/getTags";
+import { tagDetailsSelection } from "../../sanity/groqd/selections/tag-details";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const baseURL = getBaseURL();
+
+  const postSelection = pick(postDetailsSelection, ["slug", "updatedAt"]);
+  const [posts, [recentPost], categories, tags] = await Promise.all([
+    getPosts(postSelection),
+    getRecentPosts(postSelection, 1),
+    getCategories(pick(categoryDetailsSelection, ["id", "slug", "updatedAt"])),
+    getTags(pick(tagDetailsSelection, ["id", "slug", "updatedAt"])),
+  ]);
 
   return [
     {
       url: baseURL.href,
       lastModified: new Date(),
     },
+    {
+      url: new URL("/blog", baseURL).href,
+      lastModified: recentPost.updatedAt,
+    },
+    ...posts.map((post): MetadataRoute.Sitemap[number] => ({
+      url: new URL(`/blog/${post.slug}`, baseURL).href,
+      lastModified: post.updatedAt,
+    })),
+    ...(await Promise.all(
+      categories.map(
+        async (category): Promise<MetadataRoute.Sitemap[number]> => {
+          const [recentPost] = await getRecentPostsByCategoryId(
+            category.id,
+            postSelection,
+            1,
+          );
+          return {
+            url: new URL(`/blog/${category.slug}`, baseURL).href,
+            lastModified: recentPost?.updatedAt ?? category.updatedAt,
+          };
+        },
+      ),
+    )),
+    ...(await Promise.all(
+      tags.map(async (tag): Promise<MetadataRoute.Sitemap[number]> => {
+        const [recentPost] = await getRecentPostsByTagId(
+          tag.id,
+          postSelection,
+          1,
+        );
+        return {
+          url: new URL(`/blog/${tag.slug}`, baseURL).href,
+          lastModified: recentPost?.updatedAt ?? tag.updatedAt,
+        };
+      }),
+    )),
   ];
-}
+};
+
+export default sitemap;
