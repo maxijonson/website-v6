cf9de6a386bae1301312872d7720f56abe098974
Tristan Chin <tristan.chin@chintristan.io>
2024-05-13 00:48:04 -0400

    refactor Dynamic Studio

--- src/app/[...dynamicPath]/route-handlers/studioHandler.tsx
+++ src/app/[...dynamicPath]/route-handlers/studioHandler.tsx
@@ -1,7 +1,16 @@
-import StudioPage, {
-  findDatasetByPath,
-} from "../components/studio-page/studio-page";
+import { studioClient } from "../../../../sanity/utils/studio-client";
+import StudioPage from "../components/studio-page/studio-page";
 import type { DynamicRouteHandler } from "../page";
+
+const findDatasetByPath = async (path: string) => {
+  const datasets = await studioClient.datasets.list();
+  return datasets.find((dataset) => {
+    if (dataset.name === "production") {
+      return path === "studio";
+    }
+    return path === `studio-${dataset.name}`;
+  });
+};
 
 export const studioHandler: DynamicRouteHandler = {
   canHandle: async ({ params: { dynamicPath } }) => {
