69a8d045fabcaec0cda2411f7a371cc0ba26eeec
Tristan Chin <tristan.chin@chintristan.io>
2024-06-12 20:22:26 -0400

    Do not init posthog until consent has been given

--- src/app/analytics/providers/post-hog-analytics-provider.ts
+++ src/app/analytics/providers/post-hog-analytics-provider.ts
@@ -18,31 +18,7 @@ export class PostHogAnalyticsProvider extends AnalyticsProvider {
 
   public init(): void {
     ConsentApi.onConsentChange(() => this.onConsentApiEvent());
-    ConsentApi.onReady((consent) => {
-      this.onConsentApiEvent();
-      if (this.hasInit) return;
-      posthog.init(clientEnv.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: clientEnv.NEXT_PUBLIC_POSTHOG_HOST,
-        capture_pageview: false,
-        capture_pageleave: true,
-        _onCapture: (eventName, eventData) => {
-          this.log("Tracked event", eventName, eventData);
-        },
-        opt_out_capturing_by_default: consent.performance === false,
-        debug: Boolean(AnalyticsProvider.enableLogging),
-        advanced_disable_decide: true,
-        advanced_disable_feature_flags: true,
-        advanced_disable_feature_flags_on_first_load: true,
-        advanced_disable_toolbar_metrics: true,
-        disable_session_recording: true,
-        enable_heatmaps: false,
-        autocapture: false,
-        loaded: () => {
-          this.log("Initialized");
-          this.trackQueuedData();
-        },
-      });
-    });
+    ConsentApi.onReady(() => this.onConsentApiEvent());
   }
 
   public track(
@@ -70,10 +46,37 @@ export class PostHogAnalyticsProvider extends AnalyticsProvider {
   }
 
   private onConsentApiEvent() {
+    if (!this.hasInit && this.hasConsent) {
+      this.log("Initializing");
+      posthog.init(clientEnv.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: clientEnv.NEXT_PUBLIC_POSTHOG_HOST,
+        capture_pageview: false,
+        capture_pageleave: true,
+        _onCapture: (eventName, eventData) => {
+          this.log("Tracked event", eventName, eventData);
+        },
+        debug: Boolean(AnalyticsProvider.enableLogging),
+        advanced_disable_decide: true,
+        advanced_disable_feature_flags: true,
+        advanced_disable_feature_flags_on_first_load: true,
+        advanced_disable_toolbar_metrics: true,
+        disable_session_recording: true,
+        enable_heatmaps: false,
+        autocapture: false,
+        loaded: () => {
+          this.log("Initialized");
+          this.trackQueuedData();
+        },
+      });
+    }
+
+    if (!this.hasInit) return;
+
     if (this.hasConsent && !this.isOptedIn) {
       posthog.opt_in_capturing();
     } else if (!this.hasConsent && this.isOptedIn) {
       posthog.opt_out_capturing();
+      posthog.config.capture_pageleave = false;
     }
   }
 }
